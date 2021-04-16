935d57a0b0096c5d772228e7eede46667d22c11c
Nico Wawrzyniak <nwawrzyniak@techfak.uni-bielefeld.de>
2021-04-16 16:38:53 +0200

    Reverted version number from 2.0.0 [dev] to 1.3.0 for 'edit' release.

--- src/main/java/com/nwawsoft/stac/BuildData.java
+++ src/main/java/com/nwawsoft/stac/BuildData.java
@@ -1,7 +1,7 @@
 package com.nwawsoft.stac;
 
 public class BuildData {
-  public static final String VERSION = "2.0.0";
+  public static final String VERSION = "1.3.0";
   public static final String GITHUB = "GitHub";
   public static final String GITHUB_LINK = "https://github.com/JGC-Sirius/siris-trick-attempt-counter";
   public static final String WEBSITE = "siriusgg.com";
