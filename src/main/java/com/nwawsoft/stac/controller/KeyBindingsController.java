82e54113c586b7e8c5d2d012220e66e5c101e8e1
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2021-05-18 20:00:54 +0200

    Collapsed imports

--- src/main/java/com/nwawsoft/stac/controller/KeyBindingsController.java
+++ src/main/java/com/nwawsoft/stac/controller/KeyBindingsController.java
@@ -1,10 +1,7 @@
 package com.nwawsoft.stac.controller;
 
-import com.nwawsoft.stac.model.AvailableKeys;
-import com.nwawsoft.stac.model.CounterKeyListenerSingleton;
-import com.nwawsoft.stac.model.KeyBindingsFileHandler;
-import com.nwawsoft.stac.ui.KeyBindingsFrame;
-import com.nwawsoft.stac.ui.MainMenuFrame;
+import com.nwawsoft.stac.model.*;
+import com.nwawsoft.stac.ui.*;
 import com.nwawsoft.util.ui.ComponentFunctions;
 
 import javax.swing.*;
