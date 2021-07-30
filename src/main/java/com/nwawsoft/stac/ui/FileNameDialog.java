3510ec966e98129642be11263c3ebd487f2cdd34
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2021-07-30 04:05:55 +0200

    Automated line wrapping in FileNameDialog

--- src/main/java/com/nwawsoft/stac/ui/FileNameDialog.java
+++ src/main/java/com/nwawsoft/stac/ui/FileNameDialog.java
@@ -15,40 +15,39 @@ public class FileNameDialog extends JDialog {
     pack();
     setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     int frameWidth = 300;
-    int frameHeight = 180;
+    int frameHeight = 160;
     setSize(frameWidth, frameHeight);
     setLocationRelativeTo(null);
     setTitle("Illegal file name");
     Container cp = getContentPane();
     cp.setLayout(null);
 
-    String fileNameRulesPlain = "Please enter a legal file name in the second\n" +
-        "text field. Names must be non-empty and may\n" +
-        "only consist of letters from a to Z,\n" +
-        "digits from 0 to 9 and the symbols\n" +
-        "\"_\" (underscore) and \"-\" (minus)";
-    String fileNameRulesHTML = "<html><p width=\"290\">" +
-        "Please enter a legal file name in the second text field." +
-        "<br>" +
-        "Names must be non-empty and may only consist of letters from a to Z, digits from 0 to 9 and the symbols " +
-        "\"_\" (underscore) and \"-\" (minus)." +
-        "</p></html>";
+    String fileNameRulesPlain = "Please enter a legal file name in the second text field. Names must be " +
+        "non-empty and may only consist of letters from a to Z, digits from 0 to 9 and the symbols \"_\" " +
+        "(underscore) and \"-\" (minus).";
 
     int buttonReservedSpace = 70;
-    int heightSpacer = 10;
+    int heightSpacer = 20;
     int buttonWidth = 100;
     int buttonHeight = 30;
-    JTextPane textPaneMessage = new JTextPane();
-    textPaneMessage.setText(fileNameRulesPlain);
-    textPaneMessage.setBounds(0, 0, frameWidth, frameHeight - buttonReservedSpace - heightSpacer);
-    StyledDocument doc = textPaneMessage.getStyledDocument();
-    SimpleAttributeSet center = new SimpleAttributeSet();
-    StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
-    doc.setParagraphAttributes(0, doc.getLength(), center, false);
-    textPaneMessage.setEditable(false);
-    cp.add(textPaneMessage);
+    int leftRightSpacer = 10;
+    int upperSpacer = 5;
+    int rightBonusSpacer = 5;
+    int buttonOffset = 10;
+
+    JTextArea textAreaMessage = new JTextArea();
+    textAreaMessage.setText(fileNameRulesPlain);
+    textAreaMessage.setEditable(false);
+    textAreaMessage.setLineWrap(true);
+    textAreaMessage.setWrapStyleWord(true);
+    textAreaMessage.setBackground(null);
+    textAreaMessage.setBounds(leftRightSpacer / 2, upperSpacer,
+        frameWidth - leftRightSpacer - rightBonusSpacer, frameHeight - buttonReservedSpace - heightSpacer);
+    cp.add(textAreaMessage);
+
     JButton buttonOK = new JButton("OK");
-    buttonOK.setBounds((frameWidth - buttonWidth) / 2, frameHeight - buttonReservedSpace, buttonWidth, buttonHeight);
+    buttonOK.setBounds((frameWidth - buttonWidth) / 2, frameHeight - buttonReservedSpace - buttonOffset,
+        buttonWidth, buttonHeight);
     buttonOK.addActionListener(this::buttonOK_ActionPerformed);
     cp.add(buttonOK);
 
