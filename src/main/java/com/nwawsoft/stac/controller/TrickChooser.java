adcd8fec07ff548a9f3b9c93e669a8749d1c492b
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2021-07-31 22:40:35 +0200

    Cancel button in TrickChooser now works properly

--- src/main/java/com/nwawsoft/stac/controller/TrickChooser.java
+++ src/main/java/com/nwawsoft/stac/controller/TrickChooser.java
@@ -19,19 +19,21 @@ public class TrickChooser {
         ("STAC Trick File (." + TRICK_FILE_FORMAT + ")", TRICK_FILE_FORMAT);
     jfc.addChoosableFileFilter(filter);
     jfc.setFileFilter(filter);
-    jfc.showOpenDialog(null);
-    File trickFile = jfc.getSelectedFile();
-    String trickFileStringNoEnding = null;
-    if (trickFile != null && trickFile.exists()) {
-      String trickFileString = trickFile.getName();
-      if (trickFileString.endsWith("." + TRICK_FILE_FORMAT)) {
-        trickFileStringNoEnding = trickFileString.substring(0, trickFileString.indexOf('.'));
-      } else {
-        trickFileStringNoEnding = trickFileString;
+    int result = jfc.showOpenDialog(null);
+    if (result == JFileChooser.APPROVE_OPTION) {
+      File trickFile = jfc.getSelectedFile();
+      String trickFileStringNoEnding = null;
+      if (trickFile != null && trickFile.exists()) {
+        String trickFileString = trickFile.getName();
+        if (trickFileString.endsWith("." + TRICK_FILE_FORMAT)) {
+          trickFileStringNoEnding = trickFileString.substring(0, trickFileString.indexOf('.'));
+        } else {
+          trickFileStringNoEnding = trickFileString;
+        }
       }
-    }
-    if (trickFileStringNoEnding != null) {
-      return TrickFileHandler.load(trickFileStringNoEnding);
+      if (trickFileStringNoEnding != null) {
+        return TrickFileHandler.load(trickFileStringNoEnding);
+      }
     }
     return null;
   }
